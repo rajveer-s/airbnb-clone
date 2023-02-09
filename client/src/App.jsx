17969937c05a4623f686ca37558c294879dc5b31
rajveer-s <rajsidhu2924@gmail.com>
2023-02-09 11:33:50 -0800

    navbar search bar created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <header>
+      <header className="p-4 flex justify-between">
         <a href="" className="flex items-center gap-1">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -19,6 +19,29 @@ function App() {
           </svg>
           <span className="font-bold text-xl">airbnv</span>
         </a>
+        <div className="flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-400">
+          <div>Anywhere</div>
+          <div className="border-l border-gray-300"></div>
+          <div>Any Week</div>
+          <div className="border-l border-gray-300"></div>
+          <div>Add Guests</div>
+          <button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+          </button>
+        </div>
       </header>
     </div>
   );
